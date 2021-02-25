a291143b932eabf7c624d8301c17e970af8e9f3a
root <root@localhost.localdomain>
2021-02-25 16:24:18 +0300

    delete

--- yadro/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($c = OCILogon("ILYA", "1234", "localhost/XE")) {
-    echo "Successfully connected to Oracle.\n";
-} else {
-    $err = OCIError();
-    echo "Oracle Connect Error " . $err['message'];
-}
-?>
